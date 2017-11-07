3e1c27f9849fcda69f98aeabc5f0f2f7b6276882
Omar <omargeerman@gmail.com>
2017-11-07 12:14:12 +0100

    Stateless Functional Component

--- src/app.js
+++ src/app.js
@@ -9,104 +9,98 @@ class IndecisionApp extends React.Component {
         }
     }
 
-handleDeleteOptions() {
-    this.setState(() => {
-        return {
-            options: []
-        };
-    });
-}
-
-handlePick() {
-    const randomNum = Math.floor(Math.random() * this.state.options.length)
-    const option = this.state.options[randomNum];
-    console.log(option);
-    alert(option);
-}
-
-handleAddOption(option) {
-	if(!option) {
-		return 'Enter valid value to add item'
-	} else if (this.state.options.indexOf(option) > -1){
-		return 'This option already exists';
+	render() {
+			const title = 'Indecision'
+			const subtitle = 'Put your life in the hands of a computer'
+		
+			return (
+					<div>
+							<Header title = {title} subtitle={subtitle}/>
+							<Action 
+								hasOptions = {this.state.options.length >0}
+								handlePick = {this.handlePick}
+							/>
+							<Options 
+								options = {this.state.options}
+								handleDeleteOptions = {this.handleDeleteOptions} 
+							/>
+							<AddOption 
+								handleAddOption = {this.handleAddOption}
+							/>
+					</div>
+			);
 	}
 
-	this.setState((prevState) => {
-		return {
-			options: prevState.options.concat(option)
+	// Component Methods	
+	handleDeleteOptions() {
+			this.setState(() => {
+					return {
+							options: []
+					};
+			});
+	}
+
+	handlePick() {
+			const randomNum = Math.floor(Math.random() * this.state.options.length)
+			const option = this.state.options[randomNum];
+			console.log(option);
+			alert(option);
+	}
+
+	handleAddOption(option) {
+		if(!option) {
+			return 'Enter valid value to add item'
+		} else if (this.state.options.indexOf(option) > -1){
+			return 'This option already exists';
 		}
-	})
+
+		this.setState((prevState) => {
+			return {
+				options: prevState.options.concat(option)
+			}
+		})
+	}
 }
 
-    render() {
-        const title = 'Indecision'
-        const subtitle = 'Put your life in the hands of a computer'
-			
-        return (
-            <div>
-                <Header title = {title} subtitle={subtitle}/>
-                <Action 
-                  hasOptions = {this.state.options.length >0}
-                  handlePick = {this.handlePick}
-                />
-                <Options 
-                  options = {this.state.options}
-                  handleDeleteOptions = {this.handleDeleteOptions} 
-                />
-								<AddOption 
-									handleAddOption = {this.handleAddOption}
-								/>
-            </div>
-        );
-    }
+// Stateless Functional Components
+const Header = (props) => {
+	return (
+		<div>
+		<h1>{props.title}</h1>
+		<h2>{props.subtitle}</h2>
+		</div>
+	);
 }
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-            <h1>{this.props.title}</h1>
-            <h2>{this.props.subtitle}</h2>
-            </div>
-        );
-    }
+const Action = (props) => {
+	return (
+		<div>
+		<button 
+				onClick={props.handlePick}
+				disabled={!props.hasOptions}
+		>
+			What should I do</button>
+		</div>
+	);
 }
 
-class Action  extends React.Component {
-    render() {
-        return (
-            <div>
-            <button 
-                onClick={this.props.handlePick}
-                disabled={!this.props.hasOptions}
-            >
-            What should I do</button>
-            </div>
-        );
-    }
-}
+const Options = (props) =>{
+	return (
+		<div>
+				<button onClick={props.handleDeleteOptions}>Remove All</button>
+				{
+						props.options.map((option) => <Option key={option} optionText={option} />)
+				}
+		</div>
+	);
+} 
 
-class Options extends React.Component {
-    render() {
-        return (
-            <div>
-                <button onClick={this.props.handleDeleteOptions}>Remove All</button>
-                {
-                    this.props.options.map((option) => <Option key={option} optionText={option} />)
-                }
-            </div>
-        );
-    }
-}
-
-class Option extends React.Component {
-    render() {
-        return (
-            <div>
-                {this.props.optionText}
-            </div>
-        )
-    }
+const Option = (props) => {
+	return (
+		<div>
+				{props.optionText}
+		</div>
+	);
 }
 
 class AddOption extends React.Component {
@@ -140,6 +134,13 @@ class AddOption extends React.Component {
     }
 }
 
-
+// const User = () => {
+// 	return (
+// 	<div>
+// 		<p>Name: </p>
+// 		<p>Age: </p>
+// 	</div>
+// 	);
+// };
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
